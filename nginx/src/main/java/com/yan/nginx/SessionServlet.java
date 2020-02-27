0a91258dff72c8986978ca1cbc27c3e49bf9155c
Mr.Yan <2466707075@qq.com>
2020-02-27 19:14:07 +0800

    第一次提交,未修改

---
+++ nginx/src/main/java/com/yan/nginx/SessionServlet.java
@@ -0,0 +1,26 @@
+package com.yan.nginx;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Random;
+
+/**
+ * @author
+ * @date 2020/2/27 13:25
+ */
+public class SessionServlet extends HttpServlet {
+
+    private static final long serialVersionUID = -8552876250776236612L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if(req.getSession().getAttribute("uid") == null){
+            req.getSession().setAttribute("uid", new Random().nextInt(100));
+        }
+        System.out.println(req.getSession().getAttribute("uid"));
+        req.getRequestDispatcher("/index.jsp").forward(req,resp);
+    }
+}

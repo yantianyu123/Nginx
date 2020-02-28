61355d15a8f1196c22b60fe0cca8e0f650169653
Mr.Yan <2466707075@qq.com>
2020-02-28 14:42:10 +0800

    02:第一次修改：添加了输出语句“1”

--- nginx/src/main/java/com/yan/nginx/Test.java
+++ nginx/src/main/java/com/yan/nginx/Test.java
@@ -7,5 +7,6 @@ package com.yan.nginx;
 public class Test {
     public static void main(String[] args) {
         System.out.println("000000000000000");
+        System.out.println("1111111111111111");
     }
 }

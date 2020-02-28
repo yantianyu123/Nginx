5968f08f3cd39080875d54d75fbdb6d8c65ff118
Mr.Yan <2466707075@qq.com>
2020-02-28 14:53:06 +0800

    02:第二次修改：添加了输出语句“3”

--- nginx/src/main/java/com/yan/nginx/Test.java
+++ nginx/src/main/java/com/yan/nginx/Test.java
@@ -8,5 +8,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("000000000000000");
         System.out.println("1111111111111111");
+        System.out.println("3333333333333333");
     }
 }
